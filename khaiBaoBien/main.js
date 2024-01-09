a514c2271e4c030b702430df87ddd931700a2bbb
gamsa <gamntph34159@fpt.edu.vn>
2024-01-09 19:01:38 +0700

    upload let vảr conts

--- khaiBaoBien/main.js
+++ khaiBaoBien/main.js
@@ -14,6 +14,7 @@ console.log(a);
  * 2. Global scope = biến khai báo bằng var có phạm vi toàn cục.
  * 3. var có thể khai báo lại nhiều lần
  * 4. Khai báo với var có thể assign được.
+ * 5. có phạm vi hoạt động là block scope
  */
 
 function sum(a, b) {
@@ -26,10 +27,17 @@ let myName = "hoang";
 // let myName = "Nguyen"
 /**
  * let
- * 1. let không hoisting
- * 2. khai báo biến bằng let thì có phạm vi hoạt động là block scope và function scope: nghĩa là không thể gọi hàm trước khi khai báo
+ * 1. let không hoisting: nghĩa là không thể gọi hàm trước khi khai báo
+ * 2. khai báo biến bằng let thì có phạm vi hoạt động là block scope và function scope
  * 3. Không thể khai báo lại
  * 4. Được phép gán lại.
+ * 5. Không Global scope
+ * *EX: 
+ function tyGia(sotienUSA){
+  let tyGia=25000;
+    return sotienUSA * tyGia;
+ }
+ console.log(tygia); //không thể console.log tygia, phải console ở trong tyGia
  */
 
 /**
